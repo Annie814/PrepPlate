ae45fedcedf70f7a62b37b91219a0e7dc2952b64
Amy Jo <amyjojjm2001@gmail.com>
2022-03-05 21:13:08 -0800

    grocery list page draft done

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # PrepPlate
+
+### Select your preferences and get automatically personalized meal plans and grocery lists every week!
--- frontend/src/components/GroceryList/Grocery.jsx
+++ frontend/src/components/GroceryList/Grocery.jsx
@@ -1,7 +1,39 @@
 import React from "react";
 
+import styles from "./Grocery.module.css";
+import BackIcon from "../../assets/back.svg";
+import CartIcon from "../../assets/cart.svg";
+import Week from "../UI/Week/Week";
+import GroceryItem from "../UI/GroceryItem/GroceryItem";
 const Grocery = () => {
-  return <div>Grocery</div>;
+  const handleBackButtonClick = () => {
+    window.location.href = "/result";
+  };
+  return (
+    <div className={styles.container}>
+      <div className={styles.topContainer}>
+        <div onClick={handleBackButtonClick}>
+          <img src={BackIcon} alt="back" id={styles["back"]}></img>
+          <div className={styles.back}>Back</div>
+        </div>
+        <div className={styles.title}>Your grocery list for this week</div>
+        <div className={styles.weekContainer}>
+          <Week />
+        </div>
+        <img src={CartIcon} alt="cart" id={styles["cart"]} />
+      </div>
+
+      <label className={styles.date}>March 7 to March 13</label>
+      <div className={styles.groceries}>
+        <GroceryItem />
+        <GroceryItem />
+        <GroceryItem />
+        <GroceryItem />
+        <GroceryItem />
+        <GroceryItem />
+      </div>
+    </div>
+  );
 };
 
 export default Grocery;
