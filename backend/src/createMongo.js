fa1a39c756a693a5258afa1742bc2396d5e4b60f
Annie Wang <zwang103@students.cs.ubc.ca>
2022-03-05 16:00:43 -0800

    link to DB server

--- backend/src/createMongo.js
+++ backend/src/createMongo.js
@@ -1,5 +1,5 @@
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/mydb";
+var url = "mongodb+srv://cmdf2022:q7R2Gdm3kSM1JFXV@cluster0.8pqpu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
